e6f822255de68f03fe40813d1bc6221145df9b0b
ImDaveead <ImDaveead@users.noreply.github.com>
2017-05-18 17:58:50 -0400

    Fix
    it makes it so if the construction robot is not a table (which it should always be) it gets ignored

--- fireproof-bots/data-final-fixes.lua
+++ fireproof-bots/data-final-fixes.lua
@@ -1,4 +1,5 @@
 for _, bot in pairs(data.raw["construction-robot"]) do
+	bot = bot or {}
 	bot.resistances = bot.resistances or {}
 	table.insert(bot.resistances, {type = "fire", percent = 100})
 end
